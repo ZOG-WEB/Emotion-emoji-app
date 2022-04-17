eb2b1728e7639f798b54162cd46346a6c5a083e6
ZOG-WEB <79850764+ZOG-WEB@users.noreply.github.com>
2022-04-17 12:39:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ camera=document.getElementById("camera");
 Webcam.attach('#camera');
 
 
-function take_snapshot(){
+function Takesnapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"> ';
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"> ';
 
     });
 }
@@ -36,7 +36,47 @@ function speak(){
     var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
     
+}   
+
+function check(){
+    img=document.getElementById('captured_image');
+classifier.classify(img,gotResult);
 }
+
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    Prediction_1=results[0].label;
+    Prediction_2=results[1].label;
+    speak();
+ if(results[0].label=="happy"){
+     document.getElementById("update_emoji").innerHTML="&#128522;";
+ }   
+ if(results[0].label=="sad"){
+    document.getElementById("update_emoji").innerHTML="&#128532;";
+}
+if(results[0].label=="angry"){
+    document.getElementById("update_emoji").innerHTML="&#128548;";
+}
+if(results[1].label=="happy"){
+    document.getElementById("update_emoji2").innerHTML="&#128522;";
+}   
+if(results[1].label=="sad"){
+   document.getElementById("update_emoji2").innerHTML="&#128532;";
+}
+if(results[1].label=="angry"){
+   document.getElementById("update_emoji2").innerHTML="&#128548;";
+}
+}
+}
+
+
+
 
 
 
